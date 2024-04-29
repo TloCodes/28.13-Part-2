250abae422695024b5d80b6141a31e463484501e
cmarunza <167131881+cmarunza@users.noreply.github.com>
2024-04-29 17:39:54 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,39 @@
-/* Type your code here */
+#include "ShoppingCart.h"
+#include <iostream>
+#include <algorithm> // Needed for find_if
+
+ShoppingCart::ShoppingCart() : customerName("none"), currentDate("January 1, 2016") {}
+
+ShoppingCart::ShoppingCart(string name, string date)
+    : customerName(move(name)), currentDate(move(date)) {}
+
+void ShoppingCart::AddItem(ItemToPurchase item) {
+    cartItems.push_back(move(item));
+}
+
+string ShoppingCart::GetCustomerName() const {
+    return customerName;
+}
+
+string ShoppingCart::GetDate() const {
+    return currentDate;
+}
+
+void ShoppingCart::RemoveItem(string itemName) {
+    auto it = find_if(cartItems.begin(), cartItems.end(),
+                      [&itemName](const ItemToPurchase& item) {
+                          return item.GetName() == itemName;
+                      });
+
+    if (it != cartItems.end()) {
+        cartItems.erase(it);
+    } else {
+        cout << "Item not found in cart. Nothing removed." << endl;
+    }
+}
+
+void ShoppingCart::ModifyItem(ItemToPurchase item) {
+    auto it = find_if(cartItems.begin(), cartItems.end(),
+                      [&item](const ItemToPurchase& cartItem) {
+                          return cartItem.GetName() == item.GetName();
+                      });
