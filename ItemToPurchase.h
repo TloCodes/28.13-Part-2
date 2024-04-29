fed236e826cbe68392340dea8b0416244b731a90
cmarunza <167131881+cmarunza@users.noreply.github.com>
2024-04-29 17:38:47 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,4 +1,21 @@
 #ifndef ITEM_TO_PURCHASE_H
 #define ITEM_TO_PURCHASE_H
 
+#include <string>
+using std::string;  
+
+class ItemToPurchase {
+public:
+    ItemToPurchase();
+    ItemToPurchase(string name, string description, int price, int quantity);
+
+    void SetName(string name);
+    void SetDescription(string description);
+    void SetPrice(int price);
+    void SetQuantity(int quantity);
+
+    string GetName() const;
+    string GetDescription() const;
+    int GetPrice() const;
+    int GetQuantity() const;
 
