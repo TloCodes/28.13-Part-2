696710b880c24e1835e3708ac29674ebd318923e
cmarunza <167131881+cmarunza@users.noreply.github.com>
2024-04-29 17:40:19 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -37,3 +37,53 @@ void ShoppingCart::ModifyItem(ItemToPurchase item) {
                       [&item](const ItemToPurchase& cartItem) {
                           return cartItem.GetName() == item.GetName();
                       });
+if (it != cartItems.end()) {
+        if (!item.GetDescription().empty()) it->SetDescription(item.GetDescription());
+        if (item.GetPrice() != 0) it->SetPrice(item.GetPrice());
+        if (item.GetQuantity() != 0) it->SetQuantity(item.GetQuantity());
+    } else {
+        cout << "Item not found in cart. Nothing modified." << endl;
+    }
+}
+
+int ShoppingCart::GetNumItemsInCart() const {
+    int numItems = 0;
+    for (const auto& item : cartItems) {
+        numItems += item.GetQuantity();
+    }
+    return numItems;
+}
+
+double ShoppingCart::GetCostOfCart() const {
+    double totalCost = 0;
+    for (const auto& item : cartItems) {
+        totalCost += item.GetPrice() * item.GetQuantity();
+    }
+    return totalCost;
+}
+
+void ShoppingCart::PrintTotal() {
+    cout << customerName << "'s Shopping Cart - " << currentDate << endl;
+    cout << "Number of Items: " << GetNumItemsInCart() << endl << endl;
+   
+    if (cartItems.empty()) {
+        cout << "SHOPPING CART IS EMPTY" << endl << endl;
+        cout << "Total: $0" << endl;
+       
+    } else {
+        for (const ItemToPurchase& item : cartItems) {
+            item.PrintItemCost();
+        }
+        cout << endl;
+        cout << "Total: $" << GetCostOfCart() << endl;
+    }
+}
+
+void ShoppingCart::PrintDescriptions() {
+    cout << customerName << "'s Shopping Cart - " << currentDate << endl;
+    cout << endl;
+    cout << "Item Descriptions" << endl;
+    for (const ItemToPurchase& item : cartItems) {
+        item.PrintItemDescription();
+    }
+}
